dfb97a009fd9b670f3ad5a4e186ecfd578e6bebb
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-04 13:16:45 -0300

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from energy-data!")
+import pandas as pd
+import psycopg
 
+def get_dataframe(db_connection_string: str, sql_query: str):
+    try:
+        # Establish a connection to the database using psycopg3
+        # The 'with' statement ensures the connection is automatically closed.
+        with psycopg.connect(db_connection_string) as conn:
+            # Execute the query and fetch the data into a pandas DataFrame
+            df = pd.read_sql_query(sql_query, conn)
+            return df
 
-if __name__ == "__main__":
-    main()
+    except (Exception, psycopg.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
