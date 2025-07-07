313a9ce3fb1f3221741775487d99efb898aeea86
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-07 17:48:20 -0300

    [skip ci] Updated

---
+++ tests/test_fetch_dataframe.py
@@ -0,0 +1,38 @@
+import psr.lakehouse
+
+import dotenv
+import os
+import pandas as pd
+
+dotenv.load_dotenv()
+server = os.getenv("POSTGRES_SERVER")
+port = os.getenv("POSTGRES_PORT")
+db = os.getenv("POSTGRES_DB")
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+client = psr.lakehouse.Client(server, port, db, user, password)
+
+
+def test_ccee_spot_price():
+    df = client.fetch_dataframe(
+        table_name="ccee_spot_price",
+        indices_columns=["reference_date", "subsystem"],
+        data_columns=["spot_price"],
+        start_reference_date="2023-05-01 03:00:00",
+        end_reference_date="2023-05-01 04:00:00",
+    )
+    print(df)
+
+    expected_index = pd.MultiIndex.from_tuples(
+        [
+            (pd.to_datetime("2023-05-01 03:00:00"), "NORTH"),
+            (pd.to_datetime("2023-05-01 03:00:00"), "NORTHEAST"),
+            (pd.to_datetime("2023-05-01 03:00:00"), "SOUTHEAST"),
+            (pd.to_datetime("2023-05-01 03:00:00"), "SOUTH"),
+        ],
+        names=["reference_date", "subsystem"],
+    )
+    pd.testing.assert_index_equal(df.index, expected_index, check_exact=True)
+
+    expected_series = pd.Series([69.04, 69.04, 69.04, 69.04], index=expected_index, name="spot_price")
+    pd.testing.assert_series_equal(df["spot_price"], expected_series)
