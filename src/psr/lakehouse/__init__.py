bb40967f25593b5a06a938c0a10e398c693b2465
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-14 18:22:51 -0300

    Updated

--- src/psr/lakehouse/__init__.py
+++ src/psr/lakehouse/__init__.py
@@ -2,15 +2,6 @@ from .aliases import ccee as ccee, ons as ons
 from .client import client
 from .connector import connector as connector
 
+initialize = connector.initialize
 
-def initialize(
-    aws_access_key_id: str,
-    aws_secret_access_key: str,
-):
-    connector.initialize(
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-    )
-
-
-__all__ = ["client", "initialize"]
+__all__ = ["client", "connector", "initialize"]
