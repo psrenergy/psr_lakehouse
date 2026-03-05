16b49bd708c6062bfb46b7863614d44d69d26b0a
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-03-05 14:56:58 -0300

    Updated

--- src/psr/lakehouse/__init__.py
+++ src/psr/lakehouse/__init__.py
@@ -1,8 +1,11 @@
+from .alias import register_aliases
 from .client import client
 from .connector import connector as connector
 from .metadata import get_model_name
 
 initialize = connector.initialize
+
+register_aliases()
 
 __all__ = [
     "client",
